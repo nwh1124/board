80fd1422116b98ff718b93b4a7d52d8b1f460d7c
nwh1124 <nwh0716@gmail.com>
2020-12-17 18:04:38 +0900

    Db 이름 변경exam -> ssgDb, 게시판 이름 변경 JAVA -> IT

--- currunt.sql
+++ currunt.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Community v13.1.7 (64 bit)
-MySQL - 10.4.14-MariaDB : Database - exam
+MySQL - 10.4.14-MariaDB : Database - ssgDb
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 10.4.14-MariaDB : Database - exam
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`exam` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ssgDb` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
-USE `exam`;
+USE `ssgDb`;
 
 /*Table structure for table `article` */
 
@@ -59,7 +59,7 @@ CREATE TABLE `board` (
 insert  into `board`(`id`,`regDate`,`updateDate`,`name`,`code`) values 
 (1,'2020-12-17 13:12:03','2020-12-17 13:12:03','자유','free'),
 (2,'2020-12-17 13:12:03','2020-12-17 13:12:03','공지사항','notice'),
-(3,'2020-12-17 13:12:03','2020-12-17 13:12:03','Java','java');
+(3,'2020-12-17 13:12:03','2020-12-17 13:12:03','IT','it');
 
 /*Table structure for table `member` */
 
