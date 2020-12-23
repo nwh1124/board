163e70d46c96c6c793e3ed7c3dce93c7dab25c0e
nwh1124 <nwh0716@gmail.com>
2020-12-23 13:50:00 +0900

    토스트ui 인덱스에 두 개 사용

--- app.js
+++ app.js
@@ -36,4 +36,23 @@ function viewer_index() {
 	});
 }
 
+function viewer_index_2() {
+	
+	var viewerEl = document.querySelector('.main .content:nth-child(2)');
+	
+	if ( viewerEl == null ) {
+		return;
+	}
+	
+	var body = viewerEl.innerHTML;
+	
+	var viewer = new toastui.Editor.factory({
+		el : viewerEl,
+		initialValue : body,
+		viewer : true,
+	    plugins: [toastui.Editor.plugin.codeSyntaxHighlight]
+	});
+}
+
 viewer_index();
+viewer_index_2();
